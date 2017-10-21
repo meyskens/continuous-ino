5f510a940b37d656f3dd598b990e842a94d26a5b
Maartje Eyskens <meyskens@me.com>
2017-10-21 12:34:29 +0200

    test clone and presence of build file

--- build.go
+++ build.go
@@ -1,19 +1,21 @@
 package main
 
 import (
+	"errors"
 	"os"
 	"strings"
 
 	git "gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing"
 )
 
-func clone(repo string) (string, error) {
+func clone(repo, hash string) (string, error) {
 	nameParts := strings.Split(repo, "/")
 	name := nameParts[len(nameParts)-1]
 	path := "/tmp/" + name
 
 	os.RemoveAll(path)
-	_, err := git.PlainClone(path, false, &git.CloneOptions{
+	r, err := git.PlainClone(path, false, &git.CloneOptions{
 		URL:      "https://github.com/src-d/go-git",
 		Progress: os.Stdout,
 	})
@@ -21,5 +23,24 @@ func clone(repo string) (string, error) {
 		return "", err
 	}
 
-	return name, nil
+	w, err := r.Worktree()
+	if err != nil {
+		return "", err
+	}
+
+	err = w.Checkout(&git.CheckoutOptions{Hash: plumbing.NewHash(hash)})
+	if err != nil {
+		return "", err
+	}
+
+	return path, nil
+}
+
+func checkBuildFile(path string) error {
+	_, err := os.Open(path + "/.cino.yml")
+	if err != nil {
+		return errors.New(".cino.yml not present")
+	}
+
+	return nil
 }
--- github.go
+++ github.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"time"
 
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
@@ -36,10 +35,32 @@ func handleHookPayload(event string, payload github.WebHookPayload) {
 }
 
 func handlePush(payload github.WebHookPayload) {
-	// Set pending
-	gitHubClient.Repositories.CreateStatus(context.Background(), payload.Repo.Owner.GetLogin(), payload.Repo.GetName(), payload.GetAfter(), &github.RepoStatus{State: &statusPending})
+	setPending(payload)
 
-	// dummy it for now
-	time.Sleep(10 * time.Second)
+	path, err := clone(payload.Repo.GetCloneURL(), payload.GetAfter())
+	if err != nil {
+		fmt.Println(err)
+		setFailed(payload, "Could not clone repository")
+		return
+	}
+
+	err = checkBuildFile(path)
+	if err != nil {
+		setFailed(payload, err.Error())
+		return
+	}
+
+	setSuccess(payload)
+}
+
+func setPending(payload github.WebHookPayload) {
+	gitHubClient.Repositories.CreateStatus(context.Background(), payload.Repo.Owner.GetLogin(), payload.Repo.GetName(), payload.GetAfter(), &github.RepoStatus{State: &statusPending})
+}
+
+func setFailed(payload github.WebHookPayload, reason string) {
+	gitHubClient.Repositories.CreateStatus(context.Background(), payload.Repo.Owner.GetLogin(), payload.Repo.GetName(), payload.GetAfter(), &github.RepoStatus{State: &statusFailure, Context: &reason})
+}
+
+func setSuccess(payload github.WebHookPayload) {
 	gitHubClient.Repositories.CreateStatus(context.Background(), payload.Repo.Owner.GetLogin(), payload.Repo.GetName(), payload.GetAfter(), &github.RepoStatus{State: &statusSuccess})
 }
