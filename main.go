b4659dd086716f22d4927997c120da393b0e970b
Maartje Eyskens <meyskens@me.com>
2017-10-22 10:49:19 +0200

    open db file on launch

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/boltdb/bolt"
 	"github.com/labstack/echo"
 	"github.com/meyskens/continuous-ino/config"
 )
 
 var cfg config.Configuration
+var db *bolt.DB
 
 func main() {
 	fmt.Println("Contininuous Ino -- CI for Arduino")
@@ -15,6 +18,12 @@ func main() {
 
 	cfg = config.GetConfiguration()
 	setUpGitHub()
+
+	db, err := bolt.Open(cfg.Database.Path, 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
 	e := echo.New()
 	e.GET("/", serveRoot)
