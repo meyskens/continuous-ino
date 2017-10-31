fe53a94e03d9a1b1f54f8a1342d4e7cd761aba76
Maartje Eyskens <meyskens@me.com>
2017-10-31 20:09:56 +0100

    rm app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import Navigation from "./components/navigation"
 import { BrowserRouter as Router, Route } from 'react-router-dom';
